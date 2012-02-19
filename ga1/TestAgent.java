57f4d8d36c96bd6d5bfab356ebc3240b57ba3771
M@ <mattdunlap+github@gmail.com>
2012-02-18 20:38:30 -0500

    fixed embarassing ant mistake

--- ga1/TestAgent.java
+++ ga1/TestAgent.java
@@ -1,42 +1,42 @@
+
 /** Template for foraging agent
  *
  * @author matthias scheutz
  *
  */
-
 import com.grid.simulations.simworld.*;
 import com.grid.simulations.simworld.components.*;
 import com.grid.simulations.simworld.worlds.collector.*;
 import java.util.*;
 //
 public class TestAgent extends Agent_F {
-    // add your local variables here
+  // add your local variables here
 
-    // Do not change this line
-    public TestAgent(String thisID, double iX, double iY, long seed, Hashtable SchdulerObjectManagementList) {
-        super("testagent" + thisID, iX, iY, seed, SchdulerObjectManagementList);
-    }
+  // Do not change this line
+  public TestAgent(String thisID, double iX, double iY, long seed, Hashtable SchdulerObjectManagementList) {
+    super("testagent" + thisID, iX, iY, seed, SchdulerObjectManagementList);
+  }
 
-    // this function will be called automatically by the environment when agents are too
-    // close to each other; needs to return true if the agent is fighting vs false if not
-    // add your decision-making for fighting here
-    public boolean fight() {
-	return true;
-    }
+  // this function will be called automatically by the environment when agents are too
+  // close to each other; needs to return true if the agent is fighting vs false if not
+  // add your decision-making for fighting here
+  public boolean fight() {
+    return true;
+  }
 
-    // this returns true depending on whether the agent wants to consume a food source
-    // add your decision-making for eating here
-    public boolean eat() {
-	return true;
-    }
+  // this returns true depending on whether the agent wants to consume a food source
+  // add your decision-making for eating here
+  public boolean eat() {
+    return true;
+  }
 
-    // template sense function as descibed in the assignment
-    // add your code for sensing here
-    public void sense(ArrayList<Item> agents,ArrayList<Item> food) {
-    }
+  // template sense function as descibed in the assignment
+  // add your code for sensing here
+  public void sense(ArrayList<Item> agents, ArrayList<Item> food) {
+  }
 
-    // template act function as descibed in the assignment
-    // add your code for acting here (note that you can only perform one turn and one speed action at the same time
-    public void act() {
-    }
+  // template act function as descibed in the assignment
+  // add your code for acting here (note that you can only perform one turn and one speed action at the same time
+  public void act() {
+  }
 }
