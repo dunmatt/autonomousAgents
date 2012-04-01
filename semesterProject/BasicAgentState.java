c253c1e7c6e9544549de80acddc3bead597f4cb8
M@ <mattdunlap+github@gmail.com>
2012-04-01 18:32:10 -0400

    initial commit of the semester project

---
+++ semesterProject/BasicAgentState.java
@@ -0,0 +1,36 @@
+
+import com.grid.simulations.simworld.components.Agent2DState;
+
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+/**
+ *
+ * @author maxsmiley
+ */
+public class BasicAgentState extends Agent2DState {
+    public double maxDistancePerCycleX = 0;
+    public double maxDistancePerCycleY = 0;
+
+    public BasicAgentState(String agentIDName,
+            int agentIDNumber,
+            double x,
+            double y,
+            double maxDistancePerCycleX,
+            double maxDistancePerCycleY) {
+        super(agentIDName,
+	      agentIDNumber,maxDistancePerCycleX,
+	      maxDistancePerCycleY,
+                x, y);
+    }
+
+    public BasicAgentState(String agentIDName,
+            int agentIDNumber,
+            double x,
+	    double y) {
+        super(agentIDName,
+              agentIDNumber,
+                x, y);
+    }
+}
