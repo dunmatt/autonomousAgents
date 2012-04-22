d35d7e46c349eee57610f1c21a5c7b3808653293
M@ <mattdunlap+github@gmail.com>
2012-04-22 13:36:41 -0400

    added food

--- semesterProject/BasicAgentState.java
+++ semesterProject/BasicAgentState.java
@@ -10,27 +10,28 @@ import com.grid.simulations.simworld.components.Agent2DState;
  * @author maxsmiley
  */
 public class BasicAgentState extends Agent2DState {
-    public double maxDistancePerCycleX = 0;
-    public double maxDistancePerCycleY = 0;
 
-    public BasicAgentState(String agentIDName,
-            int agentIDNumber,
-            double x,
-            double y,
-            double maxDistancePerCycleX,
-            double maxDistancePerCycleY) {
-        super(agentIDName,
-	      agentIDNumber,maxDistancePerCycleX,
-	      maxDistancePerCycleY,
-                x, y);
-    }
+  public double maxDistancePerCycleX = 0;
+  public double maxDistancePerCycleY = 0;
 
-    public BasicAgentState(String agentIDName,
-            int agentIDNumber,
-            double x,
-	    double y) {
-        super(agentIDName,
-              agentIDNumber,0,0,
-                x, y);
-    }
+  public BasicAgentState(String agentIDName,
+          int agentIDNumber,
+          double x,
+          double y,
+          double maxDistancePerCycleX,
+          double maxDistancePerCycleY) {
+    super(agentIDName,
+            agentIDNumber, maxDistancePerCycleX,
+            maxDistancePerCycleY,
+            x, y);
+  }
+
+  public BasicAgentState(String agentIDName,
+          int agentIDNumber,
+          double x,
+          double y) {
+    super(agentIDName,
+            agentIDNumber, 100,100,
+            x, y);
+  }
 }
--- semesterProject/TestAgent.java
+++ semesterProject/TestAgent.java
@@ -1,106 +1,103 @@
+
 /** A simple foraging agent template
  *
  * @author matthias scheutz
  *
  */
-
 import com.grid.simulations.simworld.worlds.collector.*;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class TestAgent extends Agent_F {
-    // left public on purpose -- do not use these variables in your code!!!
-    double foodheading = 0;
-    double speedupcount = 0;
-    double maxspeed = 4;
-    Item closest = null;
+  // left public on purpose -- do not use these variables in your code!!!
 
-    // default constructor, make sure it's present
-    public TestAgent() {
-	super();
-    }    
-    
-    // standard simworld constructor
-    public TestAgent(String thisIDName, int thisIDNumber, double iX, double iY, long seed, Hashtable SchdulerObjectManagementList) {
-        super((thisIDName.isEmpty() ? "testagent" : thisIDName),
-	      thisIDNumber, iX, iY, seed, SchdulerObjectManagementList);
-    }
+  double foodheading = 0;
+  double speedupcount = 0;
+  double maxspeed = 4;
+  Item closest = null;
 
-    // this function will be called automatically by the environment when agents are too
-    // close to each other; needs to return true if the agent is fighting vs false if not
-    public boolean fight() {
-	if (closest != null) {
-	    if (closest.getAggression() > getAggression()) {
-		System.out.println(agentID + " is retreating...");
-		return false;
-	    }
-	    else {
-		System.out.println(agentID + " is fighting...");
-		return true;
-	    }
-	}
-	else {
-	    System.out.println("GOT NO CLOSEST BUT STILL FIGHTING...");
-	    return true;
-	}
-    }
+  // default constructor, make sure it's present
+  public TestAgent() {
+    super();
+  }
 
-    // this returns true depending on whether the agent wants to consume a food source
-    // add your decision-making for eating here
-    public boolean eat() {
-	System.out.println(agentID + " is eating...");
-	return true;
-    }
+  // standard simworld constructor
+  public TestAgent(String thisIDName, int thisIDNumber, double iX, double iY, long seed, Hashtable SchdulerObjectManagementList) {
+    super((thisIDName.isEmpty() ? "testagent" : thisIDName),
+            thisIDNumber, iX, iY, seed, SchdulerObjectManagementList);
+  }
 
-    // template sense function, could be abstract
-    public void sense(ArrayList<Item> agents,ArrayList<Item> food) {
-	double distance = 1000;
-	foodheading = -1;
-	for(Item f:food) {
-	    if (f.getDistance() < distance) {
-		distance = f.getDistance();
-		foodheading = f.getHeading();
-	    }
-	}
-	System.out.println(agentID + " is heading " + foodheading + " to get food at " + distance);
-	// look at the other agents, get the closest and remember it
-	closest = null;
-	for(Item a:agents) {
-	    if (a.getDistance() < distance) {
-		distance = a.getDistance();
-		closest = a;
-	    }
-	}	
+  // this function will be called automatically by the environment when agents are too
+  // close to each other; needs to return true if the agent is fighting vs false if not
+  public boolean fight() {
+    if (closest != null) {
+      if (closest.getAggression() > getAggression()) {
+        System.out.println(agentID + " is retreating...");
+        return false;
+      } else {
+        System.out.println(agentID + " is fighting...");
+        return true;
+      }
+    } else {
+      System.out.println("GOT NO CLOSEST BUT STILL FIGHTING...");
+      return true;
     }
+  }
 
-    // template act function, could be abstract
-    public void act() {
-	//System.out.println("Calling act on " + agentID);
-	// only turn if the foodheading is positive
-	if (foodheading>15 && foodheading <185) {
-	    turnright();
-	    if (speedupcount > 0) {
-		speedupcount--;
-		slowdown();
-		System.out.println(agentID + " is slowing down...");
-	    }
-	}
-	else if (foodheading < 345) {
-	    turnleft();
-	    if (speedupcount > 0) {
-		speedupcount--;
-		slowdown();
-		System.out.println(agentID + " is slowing down...");
-	    }
-	}
-	else if (speedupcount < maxspeed) {
-	    speedup();
-	    speedupcount++;
-	    System.out.println(agentID + " is speeding up...");
-	}
-	// compute aggression level based on "hunger"
-	System.out.println(agentID + " energy: " + getEnergy());	
-	setAggression(1.0/getEnergy());
-	System.out.println(agentID + " aggression: " + getAggression());
+  // this returns true depending on whether the agent wants to consume a food source
+  // add your decision-making for eating here
+  public boolean eat() {
+    System.out.println(agentID + " is eating...");
+    return true;
+  }
+
+  // template sense function, could be abstract
+  public void sense(ArrayList<Item> agents, ArrayList<Item> food) {
+    double distance = 1000;
+    foodheading = -1;
+    for (Item f : food) {
+      if (f.getDistance() < distance) {
+        distance = f.getDistance();
+        foodheading = f.getHeading();
+      }
     }
+    System.out.println(agentID + " is heading " + foodheading + " to get food at " + distance);
+    // look at the other agents, get the closest and remember it
+    closest = null;
+    for (Item a : agents) {
+      if (a.getDistance() < distance) {
+        distance = a.getDistance();
+        closest = a;
+      }
+    }
+  }
+
+  // template act function, could be abstract
+  public void act() {
+    //System.out.println("Calling act on " + agentID);
+    // only turn if the foodheading is positive
+    if (foodheading > 15 && foodheading < 185) {
+      turnright();
+      if (speedupcount > 0) {
+        speedupcount--;
+        slowdown();
+        System.out.println(agentID + " is slowing down...");
+      }
+    } else if (foodheading < 345) {
+      turnleft();
+      if (speedupcount > 0) {
+        speedupcount--;
+        slowdown();
+        System.out.println(agentID + " is slowing down...");
+      }
+    } else if (speedupcount < maxspeed) {
+      speedup();
+      speedupcount++;
+      System.out.println(agentID + " is speeding up...");
+    }
+    // compute aggression level based on "hunger"
+    System.out.println(agentID + " energy: " + getEnergy());
+    setAggression(1.0 / getEnergy());
+    System.out.println(agentID + " aggression: " + getAggression());
+  }
 }
